c0587eca84e9123d3f72289a91878a0202700b2f
lfy57557 <lfy57557@arc.com>
2025-03-19 13:51:53 +0800

    1.新增拦截器逻辑，全部拦截并刷新token 2.shop商品页面逻辑 3.shoptype存储至Redis

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOPTYPE_KEY;
 
 /**
  * <p>
@@ -17,4 +29,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IShopTypeService typeService;
+
+    @Override
+    public Result queryList() {
+        //查询Redis中数据
+        String key =  CACHE_SHOPTYPE_KEY;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        //有则返回
+        if (StrUtil.isNotBlank(shopJson)) {
+            ShopType shopType = BeanUtil.toBean(shopJson, ShopType.class);
+            return Result.ok(shopType);
+        }
+        //无则查询数据库
+        List<ShopType> typeList = typeService
+                .query().orderByAsc("sort").list();
+        //数据库无则返回
+        if (CollUtil.isEmpty(typeList)) {
+            return Result.fail("店铺类型信息异常");
+        }
+        //有则保存至Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+        return Result.ok(typeList);
+    }
 }
---
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -0,0 +1,51 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.util.Map;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringredisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringredisTemplate) {
+        this.stringredisTemplate = stringredisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1. 获取请求头中的token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            return true;
+        }
+        //2.基于token获取redis中的用户
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = stringredisTemplate.opsForHash().entries(key);
+        //3.判空
+        if (userMap.isEmpty()) {
+            return true;
+        }
+        //将数据转为UserDto
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        //4.存在 保存信息到ThreadLocal
+        UserHolder.saveUser(userDTO);
+
+        //刷新token有效期
+        stringredisTemplate.expire(key, Duration.ofMinutes(30));
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
